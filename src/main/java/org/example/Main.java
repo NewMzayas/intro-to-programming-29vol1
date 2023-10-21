0bc821b336f21698d208050b5d5efbb2ad1e7be4
Marje <marjezayas@gmail.com>
2023-10-21 11:31:42 +0300

    My first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // some useful shortcut
+        // line duplication
+        // mac: cdm + d
+        // windows: ctrl + d
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        //System.out.println("I'm Marje");
+//        System.out.println("I'm Marje");
+  }
+}
