2ef4902fd79db7efa0ae357750c8644d7056bd0f
Marje <marjezayas@gmail.com>
2023-10-22 09:41:57 +0300

    If/else example - started

---
+++ src/main/java/org/example/IfElseExample.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class IfElseExample {
+
+    public static void main(String[] args) {
+        System.out.println("Let's make a decision. Are you an adult or a kid?");
+
+        // if I am an adult print "I'm an adult"
+        // otherwise print "Just a kid"
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("I'm an adult");
+        }
+
+        if (age < 18) {
+            System.out.println("Just a kid.");
+        }
+        // code block marked with pair of curly brackets is like one instruction
+        if (age >= 18) {
+            System.out.println("If it is true");
+            System.out.println("Adult");
+        } else {
+            System.out.println("Else, if is it false");
+            System.out.println("kid");
+
+        }
+    }
+}
