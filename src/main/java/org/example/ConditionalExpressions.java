bcf093f762e27079865bb9ba6dd541936d4d8f31
Marje <marjezayas@gmail.com>
2023-10-22 09:42:17 +0300

    not sure what changed

---
+++ src/main/java/org/example/ConditionalExpressions.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class ConditionalExpressions {
+
+    public static void main(String[] args) {
+        System.out.println("Conditional expressions and operators");
+
+        boolean condition = false;
+        int age = 17;
+
+        boolean isAdult = age >= 18;
+        boolean isKid = age < 18;
+
+        System.out.println("Is adult: " + isAdult);
+        System.out.println("Is kid: " +isKid);
+
+        boolean negationValue = !isAdult;
+        System.out.println("negation value: " + negationValue);
+
+
+
+    }
+}
